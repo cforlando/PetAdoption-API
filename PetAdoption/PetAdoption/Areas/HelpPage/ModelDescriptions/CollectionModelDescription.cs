69885fef047797ab452aa15b87d41755bc2938ae
lmoraguez <lmoraguez@kissimmee.org>
2015-08-13 14:34:42 -0400

    Default App Scaffolding

---
+++ PetAdoption/PetAdoption/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace PetAdoption.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
