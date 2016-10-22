f2c51b08e65729b9b4dc9fdb8bddb6d451b2194d
Khalid Hoffman <khalidhoffman@gmail.com>
2016-10-22 16:55:19 -0400

    increases test timeouts

--- test.js
+++ test.js
@@ -3,5 +3,6 @@ var SpecReporter = require('jasmine-spec-reporter');
 
 var jrunner = new Jasmine();
 jrunner.addReporter(new SpecReporter());       // add jasmine-spec-reporter
+jrunner.DEFAULT_TIMEOUT_INTERVAL = 10 * 1000;
 jrunner.loadConfigFile("spec/support/jasmine.json");    // load jasmine.json configuration
 jrunner.execute();
