d401eea798b729a189f55a7e9f272e07bdae556c
Khalid Hoffman <khalidhoffman@gmail.com>
2016-10-28 14:52:21 -0400

    moves test file to bin directory

---
+++ bin/test.js
@@ -0,0 +1,13 @@
+var path = require('path'),
+    
+    Jasmine = require('jasmine'),
+    SpecReporter = require('jasmine-spec-reporter');
+
+var jrunner = new Jasmine();
+jrunner.addReporter(new SpecReporter({
+    displayStacktrace: 'all',
+    displaySpecDuration: true
+}));
+jrunner.DEFAULT_TIMEOUT_INTERVAL = 10 * 1000;
+jrunner.loadConfigFile(path.join(process.cwd(), "spec/support/jasmine.json"));
+jrunner.execute();
--- test.js
+++
@@ -1,8 +0,0 @@
-var Jasmine = require('jasmine');
-var SpecReporter = require('jasmine-spec-reporter');
-
-var jrunner = new Jasmine();
-jrunner.addReporter(new SpecReporter());       // add jasmine-spec-reporter
-jrunner.DEFAULT_TIMEOUT_INTERVAL = 10 * 1000;
-jrunner.loadConfigFile("spec/support/jasmine.json");    // load jasmine.json configuration
-jrunner.execute();
