865bda84ddeae54377944d6a1a06f7cb018c5bcc
Khalid Hoffman <khalidhoffman@gmail.com>
2017-01-09 17:56:28 -0500

    adds more scripts, env vars

--- redeploy.sh
+++ redeploy.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 sudo su
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
 echo "$(date): Checking for updates. User is $(whoami)."
-cd /home/ubuntu/PetAdoption-API
+cd $SCRIPTPATH
 git fetch
 if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
 	echo "New commits on remote."
 	echo "Pulling remote."
 	git pull origin
 	echo "Restarting services."
-	/home/ubuntu/PetAdoption-API/restart_services.sh
+        $SCRIPTPATH/restart_services.sh
 	echo "All done."
 else
 	echo "Current with remote."
--- start_services.sh
+++ start_services.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+source ./envvars.sh
 docker-compose up --build -d
