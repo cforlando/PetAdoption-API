c4653edc7e0ad9a6b7c46c9e97e7282fda81c849
Khalid Hoffman <khalidhoffman@gmail.com>
2017-07-29 19:11:10 -0400

    updates package.json. implements force exit on test completion

--- bin/test.js
+++ bin/test.js
@@ -23,7 +23,5 @@ testDirFiles.forEach(function (file) {
 
 // Run the tests.
 mocha.run(function (failures) {
-    process.on('exit', function () {
-        process.exit(failures);  // exit with non-zero status if there were failures
-    });
+    process.exit(failures);  // exit with non-zero status if there were failures
 });
